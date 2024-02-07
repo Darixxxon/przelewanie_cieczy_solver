6ff24af09f14a191342350a159debf04893e4595
Darixxxon <114952716+Darixxxon@users.noreply.github.com>
2024-02-07 17:49:40 +0100

    finished main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import copy
-import sys
-sys.setrecursionlimit(5000)
+import os
 
 def check_completed(board):
     if len(board) == 4:
@@ -8,18 +7,17 @@ def check_completed(board):
             return True
     return False
 
-
 def looking_for_moves(board, completed, moves):
     for i in range(len(board)):
         if check_completed(board[i]) and completed.count(i) == 0:
             completed.append(i)
-    
-    if len(completed) == 12:
-        return
+    completed_copy = []
     for i in range(len(board)):
         if completed.count(i) != 0:
             continue
         for j in range(len(board)):
+            if len(completed_copy) == 12:
+                return board_copy, completed_copy, moves_copy
             board_copy = copy.deepcopy(board)
             moves_copy = moves.copy()
             completed_copy = completed.copy()
@@ -40,15 +38,10 @@ def looking_for_moves(board, completed, moves):
                 for k in range(check_height(board_copy[i])):
                     board_copy[j].append(board_copy[i].pop(-1))
                 moves_copy.append(str(i+1) + " to " + str(j+1))
-                looking_for_moves(board_copy, completed_copy, moves_copy)
+                board_copy, completed_copy, moves_copy = looking_for_moves(board_copy, completed_copy, moves_copy)
                 if len(completed_copy) == 12:
-                    moves = moves_copy.copy()
-                    completed = completed_copy.copy()
-                    return
-    if len(completed) == 12:
-        moves = moves_copy.copy()
-        completed = completed_copy.copy()
-
+                    return board_copy, completed_copy, moves_copy
+    return board, completed, moves
 
 def check_height(board):
     count = 1
@@ -59,38 +52,72 @@ def check_height(board):
             return count
     return count
 
+def print_colours():
+    print("lg = 'light green'")
+    print("nv = 'navy'")
+    print("br = 'brown'")
+    print("lb = 'light blue'")
+    print("cm = 'camo'")
+    print("rd = 'red'")
+    print("gr = 'grey'")
+    print("yl = 'yellow'")
+    print("pk = 'pink'")
+    print("dg = 'dark green'")
+    print("mg = 'magenta'")
+    print("db = 'dark blue'")
+    print("' ' = 'empty'")
 
-lg = "light green"
-nv = "navy"
-br = "brown"
-lb = "light blue"
-cm = "camo"
-rd = "red"
-gr = "grey"
-yl = "yellow"
-pk = "pink"
-dg = "dark green"
-mg = "magenta"
-db = "dark blue"
-
-board = [[br, br, nv, lg],
-         [gr, rd, cm, lb],
-         [dg, pk, lb, yl],
-         [yl, pk, cm, yl],
-         [lg, mg, pk, nv],
-         [nv, dg, nv, mg],
-         [db, pk, lb, dg],
-         [db, cm, lb, db],
-         [lg, br, gr, rd],
-         [rd, gr, mg, dg],
-         [lg, db, mg, gr],
-         [yl, cm, br, rd],
-         [],
-         []]
-
+def swap_to_colours(tmp):
+    match tmp:
+        case "lg":
+            return "light green"
+        case "nv":
+            return "navy"
+        case "br":
+            return "brown"
+        case "lb":
+            return "light blue"
+        case "cm":
+            return "camo"
+        case "rd":
+            return "red"
+        case "gr":
+            return "grey"
+        case "yl":
+            return"yellow"
+        case "pk":
+            return "pink"
+        case "dg":
+            return "dark green"
+        case "mg":
+            return "magenta"
+        case "db":
+            return "dark blue"
+        case " ":
+            return "empty"
+        case _:
+            return "error"
+            
+os.system('cls')
+number_of_vials = int(input("Hello, write number of total vials: "))
+board = [[] for _ in range(number_of_vials)]
+for i in range(number_of_vials):
+    for j in range(4):
+        os.system('cls')
+        print("Use only these acronyms: ")
+        print_colours()
+        tmp = input("Write colour from the " + str(i+1) + " column and from the " + str(j+1) + " place starting from the bottom")
+        tmp = swap_to_colours(tmp)
+        if tmp == "error":
+            print("Incorrect input please retry")
+            j=-1
+            continue
+        if tmp != "empty":
+            board[i].append(tmp)
 completed = []
 moves = []
-moves_final = looking_for_moves(board, completed, moves)
+board, completed, moves = looking_for_moves(board, completed, moves)
 
-for i in range(len(moves_final)):
-    print(moves_final[i])
+os.system('cls')
+for i in range(len(moves)):
+    print(moves[i])
